18bdf46364ff977a7a40bf3f86464656836423bb
vanTam <tamngason2004@gmail.com>
2026-01-08 11:58:37 +0700

    thongke

--- be/src/main/java/com/sd20201/datn/core/admin/statistics/controller/AdStatisticsController.java
+++ be/src/main/java/com/sd20201/datn/core/admin/statistics/controller/AdStatisticsController.java
@@ -1,8 +1,17 @@
 package com.sd20201.datn.core.admin.statistics.controller;
 
+import com.sd20201.datn.core.admin.discounts.discountDetail.model.respone.AdProductRespone;
+import com.sd20201.datn.core.admin.statistics.model.response.AdInvoiceResponse;
+import com.sd20201.datn.core.admin.statistics.service.impl.AdStatisticsServiceImpl;
+import com.sd20201.datn.core.common.base.ResponseObject;
 import com.sd20201.datn.infrastructure.constant.MappingConstants;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AdStatisticsController {
 
+    @Autowired
+    private AdStatisticsServiceImpl adStatisticsService;
+
+
+    @GetMapping
+    public ResponseObject<Page<AdProductRespone>> getAllHotProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return adStatisticsService.getAllProducts(PageRequest.of(page, size));
+    }
+
+    @GetMapping("/invoice")
+    public ResponseObject<Page<AdInvoiceResponse>> getAllInvoices(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return adStatisticsService.getAllInvoice(PageRequest.of(page, size));
+    }
 
 }
--- be/src/main/java/com/sd20201/datn/core/admin/statistics/service/impl/AdStatisticsServiceImpl.java
+++ be/src/main/java/com/sd20201/datn/core/admin/statistics/service/impl/AdStatisticsServiceImpl.java
@@ -1,8 +1,37 @@
 package com.sd20201.datn.core.admin.statistics.service.impl;
 
+import com.sd20201.datn.core.admin.discounts.discountDetail.model.respone.AdProductRespone;
+import com.sd20201.datn.core.admin.statistics.model.response.AdInvoiceResponse;
+import com.sd20201.datn.core.admin.statistics.repository.AdInvoiceRepository;
 import com.sd20201.datn.core.admin.statistics.service.AdStatisticsService;
+import com.sd20201.datn.core.common.base.ResponseObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdStatisticsServiceImpl implements AdStatisticsService {
+
+    @Autowired
+    private AdInvoiceRepository adInvoiceRepository;
+
+    @Override
+    public ResponseObject<Page<AdProductRespone>> getAllProducts(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public ResponseObject<Page<AdInvoiceResponse>> getAllInvoice(Pageable pageable) {
+        Page<AdInvoiceResponse> page = adInvoiceRepository.getAllInvoice(pageable);
+
+        return new ResponseObject<>(
+                page,
+                HttpStatus.OK,
+                "Lấy danh sách hoá đơn thành công. Tổng số sản phẩm: " + page.getTotalElements(),
+                true,
+                null
+        );
+    }
 }
