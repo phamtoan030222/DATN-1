e626e0ccdec21373c0275fbeed4c6438f1b9a964
Vương Đắc Tài <taivuongdac@gmail.com>
2026-01-15 23:08:14 +0700

    sửa conflict

---
+++ be/src/main/java/com/sd20201/datn/core/admin/invoices/invoice/repository/AdInvoiceRepository1.java
@@ -0,0 +1,7 @@
+package com.sd20201.datn.core.admin.invoices.invoice.repository;
+
+import com.sd20201.datn.repository.InvoiceRepository;
+
+public interface AdInvoiceRepository1 extends InvoiceRepository {
+
+}
